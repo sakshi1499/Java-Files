3925e430487897c548262f608f6b8aed8da645a1
Sakshi Munjal <42520146+sakshi1499@users.noreply.github.com>
2020-10-06 20:49:08 +0530

    Create P_try.java

---
+++ P_try.java
@@ -0,0 +1,14 @@
+import MyPackage.*;
+
+public class P_try
+{
+   public static void main(String args[]) 
+   {      
+      String name = "Sakshi";
+      First obj = new First();
+      obj.getNames("Sakshi From Other World");
+      Second obj1 = new Second();
+      obj1.secondsub();
+      obj1.secondsub2();
+   }
+}
